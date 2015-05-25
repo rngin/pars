73d0e71109f21d878fe89ae400a8305c93bef758
mohsen <m@rngin.com>
2015-05-25 23:42:00 +0200

    server - mysql test

--- server/run.py
+++ server/run.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+
 __author__ = 'mohsen'
 
 print "starting server.."
+
+import _mysql
+import sys
+
+try:
+    con = _mysql.connect('localhost', 'mohsen', 'grBP6KcrWhoVWgazR8yKoAQR8hTjg', 'pars')
+
+    con.query("SELECT VERSION()")
+    result = con.use_result()
+
+    print "MySQL version: %s" % \
+        result.fetch_row()[0]
+
+except _mysql.Error, e:
+
+    print "Error %d: %s" % (e.args[0], e.args[1])
+    sys.exit(1)
+
+finally:
+
+    if con:
+        con.close()
