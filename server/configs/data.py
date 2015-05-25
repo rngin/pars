cd9032e0551c66db1f148d41bf39a65db47a6cd1
mohsen <m@rngin.com>
2015-05-25 23:58:45 +0200

    server - mysql test

---
+++ server/configs/data.py
@@ -0,0 +1,8 @@
+__author__ = 'mohsen'
+
+# MYSQL:
+
+HOST = 'localhost'
+USER_NAME = 'root'
+DATA_BASE = 'dev1_pars'
+PASSWORD = 'ejZKqMzM2F3tcoTigqXhfGGWg926Nc4u9g'
--- server/run.py
+++ server/run.py
@@ -6,20 +6,23 @@ print "starting server.."
 
 import _mysql
 import sys
+from configs import data
 
+
+# MYSQL DATABASE CHECK
 try:
-    con = _mysql.connect('localhost', 'root', 'ejZKqMzM2F3tcoTigqXhfGGWg926Nc4u9g', 'dev1_pars')
+    con = _mysql.connect(data.HOST, data.USER_NAME, data.PASSWORD, data.DATA_BASE)
 
     con.query("SELECT VERSION()")
     result = con.use_result()
 
 
-    print "MySQL version: %s" % \
+    print "[OK] MySQL version: %s" % \
         result.fetch_row()[0]
 
 except _mysql.Error, e:
 
-    print "Error %d: %s" % (e.args[0], e.args[1])
+    print "[ERROR] Error %d: %s" % (e.args[0], e.args[1])
     sys.exit(1)
 
 finally:
